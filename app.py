2620ae9a642bd9dc61da5f800c9772eba124d2ed
chandramouliv-web <chandramouli.v@graas.ai>
2026-06-19 13:46:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,27 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("🏷️ PUMA Campaign & Voucher Management Tool")
+st.title("🏷️ PUMA Campaign Tool")
 
-st.success("Application Started Successfully")
+col1,col2 = st.columns(2)
+
+with col1:
+    region = st.selectbox(
+        "Region",
+        ["PH","MY","SG"]
+    )
+
+with col2:
+    marketplace = st.selectbox(
+        "Marketplace",
+        [
+            "Lazada",
+            "Shopee",
+            "Zalora",
+            "TikTok"
+        ]
+    )
+
+st.success(
+    f"{region} | {marketplace}"
+)
